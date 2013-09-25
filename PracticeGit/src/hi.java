afce18129d8b8941ec22bbe10ffce6ba1b677ae4
zeitzbach <zeitzbach@Gmail.com>
2013-09-25 12:05:08 +0700

    PUSHED

--- PracticeGit/src/hi.java
+++ PracticeGit/src/hi.java
@@ -2,6 +2,6 @@
 public class hi {
 	public static void main(String[] args) {
 		System.out.println("Hi");
-		System.out.println("Added this part for merge test");
+		System.out.println("Added this part for merge test 2");
 	}
 }
