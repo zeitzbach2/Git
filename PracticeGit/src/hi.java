56289be36738f9a22977eb156e7352ccd85ca7f2
zeitzbach <zeitzbach@Gmail.com>
2013-09-25 12:16:20 +0700

    Hi again

--- PracticeGit/src/hi.java
+++ PracticeGit/src/hi.java
@@ -3,5 +3,6 @@ public class hi {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		System.out.println("Added this part for merge test 2");
+		System.out.println("In which I say hi back");
 	}
 }
